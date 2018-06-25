0a9b9842f13af4a1272a11bcecffaaf922261d8a
bloodstalker <thabogre@gmail.com>
2018-06-25 18:40:21 +0430

    update

--- mausgaenger.c
+++ mausgaenger.c
@@ -49,9 +49,37 @@ int mouse_gaenger(char* _id, char* file_r) {
   return 0;
 }
 
+void arg_parser(cli_arg_t* cli_arg, int argc, char** argv) {
+  unsigned char dual = 0;
+  cli_arg->help = 0;
+  cli_arg->gang = 0;
+  cli_arg->dump = 0;
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "--out") == 0) {strcpy(cli_arg->out, argv[i+1]);dual = 1;}
+    else if (strcmp(argv[i], "--outb") == 0) {strcpy(cli_arg->outb, argv[i+1]);dual = 1;}
+    else if (strcmp(argv[i], "--event-id") == 0) {strcpy(cli_arg->event_id, argv[i+1]);dual = 1;}
+    else if (strcmp(argv[i], "--dump") == 0) {cli_arg->dump = 1; dual = 0;}
+    else if (strcmp(argv[i], "--gang") == 0) {cli_arg->gang = 1; dual = 0;}
+    else if ((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "-h") == 0)) {printf("fuck\n");cli_arg->help = 1; dual = 0;}
+    else {printf("unknown cli arg passed:%s.\n", argv[i]);}
+    if (dual) {i++;dual = 0;}
+  }
+}
+
 #pragma weak main
 int main (int argc, char** argv) {
+  cli_arg_t cli_arg; 
+  arg_parser(&cli_arg, argc, argv);
   //signal(SIGINT, sigint_handler);
-  return mouse_watch(argv[1], argv[2], argv[3], 0);
-  //return mouse_gaenger(argv[1], argv[2]);
+  if (cli_arg.help) {
+    printf("--out | text output file name.\n");
+    printf("--outb | binary output file name.\n");
+    printf("--event-id | mouse event file's id, e.g. 6 in event6\n");
+    printf("--dump | dump text output to stdout.\n");
+    printf("--gang | impersonate the mouse by writing to the corresponding event file.\n");
+    printf("--help | -h | display help.\n");
+    return 0;
+  }
+  if (cli_arg.gang) return mouse_gaenger(cli_arg.event_id, cli_arg.outb);
+  else return mouse_watch(cli_arg.event_id, cli_arg.out, cli_arg.outb, cli_arg.dump);
 }
--- mausgaenger.h
+++ mausgaenger.h
@@ -1,6 +1,15 @@
 
 #ifndef _MOUSE_DOPP_H
 #define _MOUSE_DOPP_H 
+typedef struct {
+  char out[256];
+  char outb[256];
+  char event_id[8];
+  unsigned char dump;
+  unsigned char gang;
+  unsigned char help;
+}cli_arg_t;
+void arg_parser(cli_arg_t* cli_arg, int argc, char** argv);
 void sigint_handler(int dummy);
 int mouse_watch(char* _id, char* filename, char* fileb, unsigned char dump);
 int mouse_gaenger(char* _id, char* file_r);
